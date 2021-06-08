b2589eaafbfc00603799d98e431fa5907d19214d
Sine <sinje@aarhus.dk>
2021-06-08 15:05:34 +0200

    AR-499: added momentjs and fixed test

--- src/debug-bar.js
+++ src/debug-bar.js
@@ -141,7 +141,7 @@ function DebugBar() {
             </button>
             <select onChange={handleChange}>
               {fixtures.map((fixture) => (
-                <option id={fixture.title}>{fixture.title}</option>
+                <option id={fixture.title} key={fixture.file}>{fixture.title}</option>
               ))}
             </select>
           </div>
