0321ef3e43a45eafaf84a89efa63c8b49a6d4a00
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2021-06-08 06:54:12 +0200

    AR-517: Added winston logger

---
+++ src/logger/logger.js
@@ -0,0 +1,22 @@
+const winston = require('winston');
+
+// @TODO: Enabled configuration of the logger.
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    // @TODO: Add production logging transports.
+  ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  );
+}
+
+module.exports = logger;
