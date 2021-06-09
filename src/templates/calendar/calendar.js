8f60df6683ba4f38b12d30880b3ae48397847d2a
Sine <sinje@aarhus.dk>
2021-06-09 10:39:20 +0200

    AR-499: added a test and made datetime update

--- src/templates/calendar/calendar.js
+++ src/templates/calendar/calendar.js
@@ -1,4 +1,4 @@
-import { React } from 'react';
+import { React, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './calendar.scss';
 import moment from 'moment';
@@ -14,24 +14,69 @@ import 'moment/locale/da';
  *   The component.
  */
 function Calendar({ content }) {
-  const classes = `template-calendar ${content.backgroundColor}`;
+  const { backgroundColor, hasDateAndTime, title, entries } = content;
+  const classes = `template-calendar ${backgroundColor}`;
 
-  const capitalize = (s) => {
-    return s.charAt(0).toUpperCase() + s.slice(1);
-  };
-  const date = capitalize(moment().locale('da').format('llll'));
+  /**
+   * Creates and updates the datestring.
+   *
+   * @returns {string}
+   *    Returns an updated datestring.
+   */
+  function useNewTimer() {
+    /**
+     * Capitalize the datestring, as it starts with the weekday.
+     *
+     * @param {string} s
+     *    The string to capitalize.
+     * @returns {string}
+     *    The capitalized string.
+     */
+    const capitalize = (s) => {
+      return s.charAt(0).toUpperCase() + s.slice(1);
+    };
+    const [date, setDate] = useState();
+
+    /**
+     * Updates the date string.
+     */
+    const displayTime = () => {
+      setDate(capitalize(moment().locale('da').format('llll')));
+    };
+
+    /**
+     * Has timer, that calls displaytime to update date.
+     */
+    useEffect(() => {
+      displayTime();
+      const timer = setInterval(() => displayTime(), 1000);
+      return function cleanup() {
+        clearInterval(timer);
+      };
+    });
+
+    return date;
+  }
+
+  const date = hasDateAndTime ? useNewTimer() : undefined;
 
   return (
     <div className={classes}>
       <div className="grid-container-title-date">
-        <div className="grid-item">{content.title}</div>
-        <div className="grid-item-end">{date}</div>
+        <div className="grid-item">{title}</div>
+        <div className="grid-item-end">{date && date}</div>
       </div>
       <div className="grid-container">
-        <div className="grid-item" key={1}>Hvad</div>
-        <div className="grid-item" key={2}>Hvornår</div>
-        <div className="grid-item" key={3}>Hvor</div>
-        {content.entries.map((entry) => (
+        <div className="grid-item" key={1}>
+          Hvad
+        </div>
+        <div className="grid-item" key={2}>
+          Hvornår
+        </div>
+        <div className="grid-item" key={3}>
+          Hvor
+        </div>
+        {entries.map((entry) => (
           <>
             <div className="grid-item" key={entry.what}>
               {entry.what}
@@ -59,7 +104,8 @@ Calendar.propTypes = {
       })
     ),
     title: PropTypes.string.isRequired,
-    backgroundColor: PropTypes.string.isRequired
+    backgroundColor: PropTypes.string.isRequired,
+    hasDateAndTime: PropTypes.bool
   }).isRequired
 };
 
