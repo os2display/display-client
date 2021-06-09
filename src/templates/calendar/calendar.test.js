a0efc970a126518cd13e82de1e122ec6943db95f
Sine <sinje@aarhus.dk>
2021-06-09 10:39:20 +0200

    AR-499: added a test and made datetime update

--- src/templates/calendar/calendar.test.js
+++ src/templates/calendar/calendar.test.js
@@ -2,10 +2,15 @@ describe('Make sure calendar loads', () => {
   it('should load component', () => {
     cy.visit('http://localhost:3000');
     cy.get('select').select('calendar1');
-    cy.get('.template-calendar').should(
-      'have.css',
-      'background-color',
-      'rgb(35, 85, 135)'
-    );
+    cy.get('.template-calendar').should('have.css', 'background-color', 'rgb(35, 85, 135)');
+  });
+
+  it('should have title', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('select').select('calendar1');
+    cy.get('.grid-item')
+      .first()
+      .invoke('text')
+      .should('match', /^Calendar 1/);
   });
 });
