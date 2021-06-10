98477b71217fe33dc0e9a709efd30de365638f29
Sine <sinje@aarhus.dk>
2021-06-10 07:52:05 +0200

    AR-499: added class to debug bar select for tests

--- src/playlist.test.js
+++ src/playlist.test.js
@@ -1,7 +1,7 @@
 describe('Render playlist', () => {
   it('Should contain Playlist divs', () => {
     cy.visit('http://localhost:3000');
-    cy.get('select').select('slideshow2');
+    cy.get('.debug-select').select('slideshow2');
     cy.get('.Playlist').should('not.be.empty');
   });
 });
--- src/region.test.js
+++ src/region.test.js
@@ -1,7 +1,7 @@
 describe('Render region', () => {
   it('Should contain Region div', () => {
     cy.visit('http://localhost:3000');
-    cy.get('select').select('slideshow2');
+    cy.get('.debug-select').select('slideshow2');
     cy.get('.Region').should('not.be.empty');
   });
 });
