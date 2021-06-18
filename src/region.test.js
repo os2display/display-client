ba0c06e1213fe9bdb62c0ee30d88e25dfb9c6856
Jesper Kristensen <cableman@linuxdev.dk>
2021-06-18 10:58:23 +0200

    AR-496: Added nginx and cypress to docker compose

--- src/region.test.js
+++ src/region.test.js
@@ -1,6 +1,6 @@
 describe('Render region', () => {
   it('Should contain Region div', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
     cy.get('.debug-select').select('slideshow2');
     cy.get('.Region').should('not.be.empty');
   });
--- src/screen.test.js
+++ src/screen.test.js
@@ -1,6 +1,6 @@
 describe('Render screen', () => {
   it('Should contain Screen div', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
     cy.get('.debug-select').select('slideshow2');
     cy.get('.Screen').should('not.be.empty');
   });
