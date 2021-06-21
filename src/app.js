f046653dc846eeb905e035c20f7439e84843b017
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2021-06-21 10:15:08 +0200

    AR-491: Added documentation

--- src/app.js
+++ src/app.js
@@ -11,18 +11,26 @@ import './app.scss';
 function App() {
   const [screen, setScreen] = useState('');
 
-  useEffect(function initialize() {
-    document.addEventListener(
-      'screen',
-      function screenEvent(event) {
-        const screenData = event.detail?.screen;
+  /**
+   * Handles "screen" events.
+   *
+   * @param {CustomEvent} event
+   *   The event.
+   */
+  function screenHandler(event) {
+    const screenData = event.detail?.screen;
 
-        if (screenData !== null) {
-          setScreen(screenData);
-        }
-      },
-      false
-    );
+    if (screenData !== null) {
+      setScreen(screenData);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('screen', screenHandler);
+
+    return function cleanup() {
+      document.removeEventListener('screen', screenHandler);
+    };
   }, []);
 
   return <div className="App">{screen && <Screen screen={screen} />}</div>;
