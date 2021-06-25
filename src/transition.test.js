bf2de5d9417daa8bc56b83ca749e9fa9df46f462
Sine <sinje@aarhus.dk>
2021-06-25 11:39:57 +0200

    AR-507: linting

--- src/transition.test.js
+++ src/transition.test.js
@@ -2,7 +2,6 @@ describe('Make sure transition loads', () => {
   it('Should have transition', () => {
     cy.visit('/');
     cy.get('#startDataSync').click();
-    cy.get('.transition-component')
-      .should('have.css', 'animation');
+    cy.get('.transition-component').should('have.css', 'animation');
   });
 });
