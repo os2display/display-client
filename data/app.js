d937ab69285942951de0c9f1944f41edae4d6296
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2021-09-13 14:33:36 +0200

    AR-642: Moved fixtures to json-server

--- data/app.js
+++ data/app.js
@@ -1,43 +1,39 @@
 const jsonServer = require('json-server');
+
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 const v1Router = jsonServer.router('v1.json');
 
-v1Router.db._.id = "jsonServerId";
-
-v1Router.render = (req, res) => {
-  let data = res.locals.data;
+const hydraRender = (req, res) => {
+  const { data } = res.locals;
 
   if (Array.isArray(data)) {
     const path = req.originalUrl;
 
-    // Remove jsonServerId.
-    res.locals.data = res.locals.data.map((element) => {
-      delete element.jsonServerId;
-      return element;
-    });
-
     res.jsonp({
-      "@id": path,
-      "@type": "hydra:Collection",
-      "hydra:member": data,
-      "hydra:totalItems": data.length,
-      "hydra:view": {
-        "@id": path + "?page=1",
-        "@type": "hydra:PartialCollectionView",
-        "hydra:first": path + "?page=1",
-        "hydra:last": path + "?page=1",
-        "hydra:next": path + "?page=1"
-      }
-    })
-  }
-  else {
-    // Remove jsonServerId.
-    delete data.jsonServerId;
-
+      '@id': path,
+      '@type': 'hydra:Collection',
+      'hydra:member': data,
+      'hydra:totalItems': data.length,
+      'hydra:view': {
+        '@id': `${path}`,
+        '@type': 'hydra:PartialCollectionView',
+        'hydra:first': `${path}`,
+        'hydra:last': `${path}`,
+        'hydra:next': `${path}`,
+      },
+    });
+  } else {
     res.jsonp(data);
   }
-}
+};
+
+server.use('/v1/playlists/:playlistId/slides', (req, res) => {
+  res.redirect(`/v1/slides?jsonServerPlaylists=%${req.params.playlistId}%`);
+});
+
+v1Router.db._.id = 'jsonServerId';
+v1Router.render = hydraRender;
 
 server.use(middlewares);
 server.use('/v1', v1Router);
