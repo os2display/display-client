c896d7c5eee8ef3f661be033b44b42f7efefae88
Sine <sinje@aarhus.dk>
2021-09-24 11:14:26 +0200

    AR-559: corrected spelling error and fixed test

--- src/templates/contacts/contacts.test.js
+++ src/templates/contacts/contacts.test.js
@@ -1,18 +1,10 @@
-describe('Make sure book-review loads', () => {
-  it('should have background-image', () => {
-    cy.visit('/');
-    cy.get('.debug-bar-select').select('bookreview1');
-    cy.get('.image-blurry-background')
-      .should('have.css', 'background-image')
-      .and('include', '/fixtures/images/mountain1.jpeg');
-  });
-
+describe('Make sure contacts loads', () => {
   it('should have title', () => {
     cy.visit('/');
-    cy.get('.debug-bar-select').select('bookreview1');
+    cy.get('.debug-bar-select').select('/v1/slides/a97f6ec4-5278-4993-bfeb-53cded000041');
     cy.get('h1')
       .first()
       .invoke('text')
-      .should('match', /^Lorem Ipsum/);
+      .should('match', /^Kontakter/);
   });
 });
