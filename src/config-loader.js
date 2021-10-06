dded7d7a29acb91d2dbf211e20c267d682e137a7
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2021-10-06 13:41:28 +0200

    AR-682: Changed debug default to true

--- src/config-loader.js
+++ src/config-loader.js
@@ -17,7 +17,7 @@ export default class ConfigLoader {
               entryPoint: '/v1/screens/497f6eca-6276-1596-bfeb-53ceb43a6f54',
             },
           },
-          debug: false,
+          debug: true,
         };
       });
   }
