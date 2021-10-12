13c59c2132c3ba40981fbe5280058ce97b1a1d26
Jesper Kristensen <cableman@linuxdev.dk>
2021-10-12 10:36:46 +0200

    AR-679: Updated nginx config to redirect

--- infrastructure/run.sh
+++ infrastructure/run.sh
@@ -5,4 +5,4 @@ VERSION=alpha
 
 docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=itkdev/os2display-client:${VERSION} --file="Dockerfile" .
 
-docker push itkdev/os2display-clien:${VERSION}
+docker push itkdev/os2display-client:${VERSION}
