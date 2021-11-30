637433ba20aeabdef4b9c9fb369de2d2d16dc6b9
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2021-11-30 15:23:34 +0100

    AR-692: Addes scheduling to client

--- src/service/scheduleService.js
+++ src/service/scheduleService.js
@@ -1,6 +1,7 @@
 import cloneDeep from 'lodash.clonedeep';
 import sha256 from 'crypto-js/sha256';
 import Base64 from 'crypto-js/enc-base64';
+import RRule from 'rrule';
 import Logger from '../logger/logger';
 
 /**
@@ -33,18 +34,7 @@ class ScheduleService {
     }
 
     // Extract slides from playlists.
-    const slides = [];
-
-    region.forEach((playlist) => {
-      playlist?.slidesData.forEach((slide) => {
-        const newSlide = cloneDeep(slide);
-        // Execution id is the product of region, playlist and slide id, to ensure uniqueness in the client.
-        newSlide.executionId = Base64.stringify(
-          sha256(regionId + playlist['@id'] + slide['@id'])
-        );
-        slides.push(newSlide);
-      });
-    });
+    const slides = ScheduleService.findScheduledSlides(region, regionId);
 
     // Calculate a hash of the region to test if it has changed.
     const hash = Base64.stringify(sha256(JSON.stringify({ region, slides })));
@@ -79,6 +69,62 @@ class ScheduleService {
       },
     });
     document.dispatchEvent(event);
+  }
+
+  static findScheduledSlides(region, regionId) {
+    // Extract slides from playlists.
+    const slides = [];
+
+    const now = new Date();
+    const startOfDay = new Date();
+    startOfDay.setUTCHours(0, 0, 0, 0);
+    const endOfDay = new Date();
+    endOfDay.setUTCHours(23, 59, 59, 999);
+
+    region.forEach((playlist) => {
+      const { schedules } = playlist;
+
+      let occurs = true;
+
+      // If schedules are set for the playlist, do not show playlist unless a schedule is active.
+      if (schedules.length > 0) {
+        occurs = false;
+      }
+
+      schedules.forEach((schedule) => {
+        const rrule = RRule.fromString(schedule.rrule.replace('\\n', '\n'));
+        rrule.between(
+          startOfDay,
+          endOfDay,
+          true,
+          function iterator(occurrenceDate) {
+            const occurrenceEnd = new Date(
+              occurrenceDate.getTime() + schedule.duration * 1000
+            );
+
+            if (now >= occurrenceDate && now <= occurrenceEnd) {
+              occurs = true;
+              // Break the iteration.
+              return false;
+            }
+            return true;
+          }
+        );
+      });
+
+      if (occurs) {
+        playlist?.slidesData?.forEach((slide) => {
+          const newSlide = cloneDeep(slide);
+          // Execution id is the product of region, playlist and slide id, to ensure uniqueness in the client.
+          newSlide.executionId = Base64.stringify(
+            sha256(regionId + playlist['@id'] + slide['@id'])
+          );
+          slides.push(newSlide);
+        });
+      }
+    });
+
+    return slides;
   }
 }
 
