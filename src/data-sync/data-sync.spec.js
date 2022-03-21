b2882f3c24e4bd02dde7cf7f2a3d47df2373b064
Sine <sinje@aarhus.dk>
2022-03-21 13:59:28 +0100

    AR-756: removed old spec file

--- src/data-sync/data-sync.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Setup data sync', () => {
-  it('should start loading data when fixture is selected', () => {
-    cy.visit('/');
-    cy.get('.debug-bar-select').select(
-      '/v1/screens/497f6eca-6276-1596-bfeb-53ceb4000002'
-    );
-    cy.contains('Slide 1');
-  });
-});
