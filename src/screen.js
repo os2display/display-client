5c9555095e4f0fe7cf4f3efc7d0fa6bce7e0eac4
Sine <sinje@aarhus.dk>
2022-03-21 13:58:03 +0100

    AR-756: removed error

--- src/screen.js
+++ src/screen.js
@@ -20,7 +20,6 @@ function Screen({ screen }) {
   const rootStyle = {
     gridTemplateAreas: createGrid(configColumns, configRows),
   };
-  screen
 
   return (
     <div className="Screen" style={rootStyle} id={screen['@id']}>
