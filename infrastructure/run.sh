5a5285909ecf01c45c044408d98642db5a503419
Ture Gjørup <ture@ignatz.dk>
2022-05-02 13:35:13 +0200

    AR-797: Fixed build args

--- infrastructure/run.sh
+++ infrastructure/run.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+set +x
+set +e
+
+APP_RELEASE_TIMESTAMP=$(date +%s)
 APP_VERSION=develop
 VERSION=alpha
 
-docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} APP_RELEASE_TIMESTAMP=$(date +%s) --tag=itkdev/os2display-client:${VERSION} --file="Dockerfile" .
+docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --build-arg APP_RELEASE_TIMESTAMP=${APP_RELEASE_TIMESTAMP} --tag=itkdev/os2display-client:${VERSION} --file="Dockerfile" .
 
 docker push itkdev/os2display-client:${VERSION}
