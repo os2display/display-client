9474949ed0147e0b08489c18afc9b48af1b5ada4
Sine Jespersen <sinejespersen@gmail.com>
2024-06-04 10:38:50 +0200

    1541: add vite to packge, update config to solve error with winston

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,89 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
+// copy pasted from https://dev.to/kjk93/how-to-use-winston-in-vite-3fdj#the-working-solution
+export default {
   server: {
     strictPort: true,
     port: 3000,
     host: 'localhost',
   },
-});
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    {
+      name: 'fix-node-globals-polyfill',
+      setup(build) {
+        build.onResolve({ filter: /util\.js/ }, ({ path }) => ({ path }));
+      },
+    },
+  ],
+  resolve: {
+    alias: {
+      // This Rollup aliases are extracted from @esbuild-plugins/node-modules-polyfill,
+      // see https://github.com/remorses/esbuild-plugins/blob/master/node-modules-polyfill/src/polyfills.ts
+      // process and buffer are excluded because already managed
+      // by node-globals-polyfill
+      util: 'util',
+      sys: 'util',
+      events: 'rollup-plugin-node-polyfills/polyfills/events',
+      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+      path: 'rollup-plugin-node-polyfills/polyfills/path',
+      querystring: 'rollup-plugin-node-polyfills/polyfills/qs',
+      punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
+      url: 'rollup-plugin-node-polyfills/polyfills/url',
+      /** ****** Remove the string decoder below */
+      // string_decoder: "rollup-plugin-node-polyfills/polyfills/string-decoder",
+      http: 'rollup-plugin-node-polyfills/polyfills/http',
+      https: 'rollup-plugin-node-polyfills/polyfills/http',
+      os: 'rollup-plugin-node-polyfills/polyfills/os',
+      assert: 'rollup-plugin-node-polyfills/polyfills/assert',
+      constants: 'rollup-plugin-node-polyfills/polyfills/constants',
+      _stream_duplex:
+        'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
+      _stream_passthrough:
+        'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
+      _stream_readable:
+        'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
+      _stream_writable:
+        'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
+      _stream_transform:
+        'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
+      timers: 'rollup-plugin-node-polyfills/polyfills/timers',
+      console: 'rollup-plugin-node-polyfills/polyfills/console',
+      vm: 'rollup-plugin-node-polyfills/polyfills/vm',
+      zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
+      tty: 'rollup-plugin-node-polyfills/polyfills/tty',
+      domain: 'rollup-plugin-node-polyfills/polyfills/domain',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      /** ******* New line inserted */
+      inject: ['./vite-polyfills/setImmediate.js'],
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        rollupNodePolyFill(),
+      ],
+    },
+  },
+};
